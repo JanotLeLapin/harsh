1e7165280c12c8742229fecdb23485005e9a6d0e
Joseph DALY <joseph300905@gmail.com>
2025-06-12 19:25:03 +0200

    write samples to file

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <sndfile.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -21,6 +22,29 @@ h_wave_sine(h_oscillator_t *osc, const h_context_t *ctx)
 }
 
 int
+h_save_wav32(const char *filename, uint32_t sample_rate, size_t sample_count, const float *samples)
+{
+  SF_INFO sfinfo;
+  SNDFILE *f;
+
+  sfinfo.frames = sample_count;
+  sfinfo.samplerate = sample_rate;
+  sfinfo.channels = 1;
+  sfinfo.format = SF_FORMAT_WAV | SF_FORMAT_FLOAT;
+
+  f = sf_open(filename, SFM_WRITE, &sfinfo);
+  if (0 == f) {
+    perror("sf_open");
+    return -1;
+  }
+
+  sf_write_float(f, samples, sample_count);
+  sf_close(f);
+
+  return 0;
+}
+
+int
 main()
 {
   h_context_t ctx;
@@ -31,12 +55,14 @@ main()
   ctx.sr = 41000;
 
   osc.phase = 0.0f;
-  osc.freq = ctx.sr;
+  osc.freq = 440;
 
   buf = malloc(sizeof(float) * ctx.sr * 10);
   for (i = 0; i < ctx.sr * 10; i++) {
     buf[i] = h_wave_sine(&osc, &ctx);
   }
+
+  h_save_wav32("out.wav", ctx.sr, ctx.sr * 10, buf);
 
   free(buf);
   return 0;
