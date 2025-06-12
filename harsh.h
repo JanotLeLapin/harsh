c78c89c493c836e3d7229849d3a55fb17f787731
Joseph DALY <joseph300905@gmail.com>
2025-06-12 22:01:59 +0200

    utility midi functions

--- harsh.h
+++ harsh.h
@@ -1,6 +1,8 @@
 #ifndef _H_HARSH
 #define _H_HARSH
 
+#include <math.h>
+
 typedef struct {
   unsigned long sample;
   float sr;
@@ -9,6 +11,43 @@ typedef struct {
 typedef struct {
   float phase;
 } h_oscillator_t;
+
+/* util */
+inline static float
+h_freq_from_midi(char midi)
+{
+  return 440 * powf(2, ((float) midi - 69) / 12);
+}
+
+inline static char
+h_midi_from_freq(float freq)
+{
+  return 69 + 12 * log2f(freq / 440);
+}
+
+inline static char
+h_midi_from_note(const char *note)
+{
+  char sharp, octave, result;
+
+  sharp = '#' == note[1];
+  octave = note[sharp ? 2 : 1] - 48;
+
+  result = sharp;
+
+  switch (note[0]) {
+    case 'C': result += 0; break;
+    case 'D': result += 2; break;
+    case 'E': result += 4; break;
+    case 'F': result += 5; break;
+    case 'G': result += 7; break;
+    case 'A': result += 9; break;
+    case 'B': result += 11; break;
+    default: return -1;
+  }
+
+  return result + (octave + 1) * 12;
+}
 
 /* oscillators */
 float h_wave_noise();
