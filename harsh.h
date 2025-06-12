8e17270be9d1412f5c737c57d616218418c58c18
Joseph DALY <joseph300905@gmail.com>
2025-06-12 21:37:31 +0200

    typedefs before function declarations

--- harsh.h
+++ harsh.h
@@ -6,11 +6,11 @@ typedef struct {
   float sr;
 } h_context_t;
 
-/* oscillators */
 typedef struct {
   float phase;
 } h_oscillator_t;
 
+/* oscillators */
 float h_wave_noise();
 float h_wave_sine(h_oscillator_t *osc, const h_context_t *ctx, float freq);
 float h_wave_square(h_oscillator_t *osc, const h_context_t *ctx, float freq);
