1db2d9756a5897e2debbef1f53b77003d375e135
Joseph DALY <joseph300905@gmail.com>
2025-06-23 20:41:46 +0200

    fix process_osc_node

--- graph.c
+++ graph.c
@@ -26,32 +26,32 @@ process_math_node(h_hm_t *g, h_graph_node_t *node, const h_context *ctx)
 static inline void
 process_osc_node(h_hm_t *g, h_graph_node_t *node, const h_context *ctx)
 {
-  h_node_osc_t data = node->data.osc;
+  h_node_osc_t *data = &node->data.osc;
   h_graph_node_t *freq, *phase;
 
-  freq = h_hm_get(g, data.freq);
-  phase = h_hm_get(g, data.phase);
+  freq = h_hm_get(g, data->freq);
+  phase = h_hm_get(g, data->phase);
   h_graph_process_node(g, freq, ctx);
   h_graph_process_node(g, phase, ctx);
 
-  switch (data.type) {
+  switch (data->type) {
   case H_NODE_OSC_SINE:
-    node->out = sinf(data.current + phase->out);
-    data.current += 2.0f * M_PI * freq->out / ctx->sr;
-    data.current = fmod(phase->out, 2.0f * M_PI);
-    if (data.current < 0.0f) data.current += 2.0f * M_PI;
+    node->out = sinf(data->current + phase->out);
+    data->current += 2.0f * M_PI * freq->out / ctx->sr;
+    data->current = fmod(data->current, 2.0f * M_PI);
+    if (data->current < 0.0f) data->current += 2.0f * M_PI;
     break;
   case H_NODE_OSC_SQUARE:
-    node->out = (data.current < 0.5 ? -1.0f : 1.0f);
-    data.current += freq->out / ctx->sr;
-    data.current = fmodf(phase->out, 1.0f);
-    if (data.current < 0.0f) data.current += 1.0f;
+    node->out = (data->current < 0.5 ? -1.0f : 1.0f);
+    data->current += freq->out / ctx->sr;
+    data->current = fmodf(data->current, 1.0f);
+    if (data->current < 0.0f) data->current += 1.0f;
     break;
   case H_NODE_OSC_SAWTOOTH:
     node->out = (1.0f - 2.0f * phase->out);
-    data.current += freq->out / ctx->sr;
-    data.current = fmodf(data.current, 1.0f);
-    if (data.current < 0.0f) data.current += 1.0f;
+    data->current += freq->out / ctx->sr;
+    data->current = fmodf(data->current, 1.0f);
+    if (data->current < 0.0f) data->current += 1.0f;
     break;
   }
 }
