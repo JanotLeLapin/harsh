d43b0d8c47ee9b7d86bd3a24d97fc17e85b5bb33
Joseph DALY <joseph300905@gmail.com>
2025-06-26 00:25:20 +0200

    also works with crazy synth

--- web/src/audio.js
+++ web/src/audio.js
@@ -16,7 +16,9 @@ export function setupAudio(element) {
   }
 
   let synth = `(synth
-    (def output (sine :freq 440.0 :phase 0.0)))`
+  (def target-freq (+ 8000.0 (* 4000.0 (sine :freq 0.1 :phase 0.0))))
+  (def bits (* 8 0.5 (+ 1.2 (sine :freq 0.15 :phase 0.0))))
+  (def output (bitcrush (diode (sine :freq 55.0 :phase 0.0)) :target_freq (ref target-freq) :bits (ref bits))))`
 
   console.log('setting up audio')
 
