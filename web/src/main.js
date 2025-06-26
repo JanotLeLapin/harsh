ac873bed5bbbd1cc0a8a814b568a51727340d74f
Joseph DALY <joseph300905@gmail.com>
2025-06-26 10:10:30 +0200

    remove default vite stuff

--- web/src/main.js
+++ web/src/main.js
@@ -1,31 +1,15 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 import { setupAudio } from './audio.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
+    <h1>Harsh</h1>
     <div id="audio">
       <div id="editor"></div>
       <button>Generate</button>
       <canvas width="1024px" height="256px"></canvas>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
 setupAudio(document.querySelector('#audio'))
