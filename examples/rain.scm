b0e0b15f0aedd99b691764f1c7a40de38acad55f
Joseph DALY <joseph300905@gmail.com>
2025-06-26 23:57:47 +0200

    rewrite rain.scm

--- examples/rain.scm
+++ examples/rain.scm
@@ -1,2 +1,4 @@
 (synth
-  (def output (* 0.06 (sine :freq (* (+ 1600.0 (* 700.0 (sine :freq 0.1 :phase 0.0))) (+ 0.5 (* 0.5 (noise :seed 5)))) :phase (* 64.0 (sine :freq 0.1 :phase 0.0))))))
+  (def sine-freq (* (+ 1600.0 (* 700.0 (sine :freq 0.1 :phase 0.0))) (+ 0.5 (* 0.5 (noise :seed 5)))))
+  (def sine-phase (* 64.0 (sine :freq 0.1 :phase 0.0)))
+  (def output (* 0.06 (sine :freq (ref sine-freq) :phase (ref sine-phase)))))
