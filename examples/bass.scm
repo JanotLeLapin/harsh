7dcd60aaa7f47b0aa9e29455db4381f962e0041c
Joseph DALY <joseph300905@gmail.com>
2025-06-27 22:25:05 +0200

    add lowpass cutoff to bass patch

--- examples/bass.scm
+++ examples/bass.scm
@@ -1,5 +1,5 @@
 (synth
   (def noise-amp (+ 0.004 (* 0.002 (sine :freq 0.06 :phase 0.0))))
   (def bits (* 4.0 (+ 2.0 (sawtooth :freq 0.2 :phase 0.0))))
-  (def carrier (square :freq 36.0 :phase (* (ref noise-amp) (noise :seed 420.0))))
-  (def output (bitcrush (ref carrier) :target_freq 12000.0 :bits (ref bits)))))
+  (def carrier (square :freq (midi->freq 27) :phase (* (ref noise-amp) (noise :seed 420.0))))
+  (def output (lowpass 650.0 2.0 (bitcrush (ref carrier) :target_freq 12000.0 :bits (ref bits))))))
